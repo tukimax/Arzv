d5e45471a4a997dc5b15eb36eac74e0ea5107348
tukimax <109847796+tukimax@users.noreply.github.com>
2023-07-17 23:55:17 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -186,10 +186,10 @@ mkdir -p /usr/bin/xray
 mkdir -p /etc/xray
 mkdir -p /usr/local/etc/xray
 sub=$(</dev/urandom tr -dc a-z | head -c5)
-DOMAIN=zeavpn.biz.id
+DOMAIN=gratisan.me
 SUB_DOMAIN=${sub}.gratisan.me
 CF_ID=tukimangoblok@gmail.com
-CF_KEY=mo1iXoJpWbxKLlluy5hv3jnJy-Anz4FoT1iU3A_a
+CF_KEY=3f8ee99768d9ee10f1cc51afa5dab78e72293
 set -euo pipefail
 IP=$(curl -sS ifconfig.me);
 echo "Updating DNS for ${SUB_DOMAIN}..."
